a242d94d3f81d6666008d77a48029c6dc6ea369b
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-09-09 10:54:42 -0300

    corringindo a model LegislaturePeriod definindo as datas como tipo date

--- app/Models/LegislaturePeriod.php
+++ app/Models/LegislaturePeriod.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class LegislaturePeriod extends Model
 {
-    protected $fillable = [
-        'legislature_number',
-        'starts_at',
-        'ends_at'
+    protected $fillable = ['legislature_number', 'starts_at', 'ends_at'];
+
+    protected $casts = [
+        'starts_at' => 'date',
+        'ends_at' => 'date',
     ];
 }
