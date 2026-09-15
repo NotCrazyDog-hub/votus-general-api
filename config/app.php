ebecf955aec2df1157c1b1b9381dc40e63b7f8ba
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-09-15 16:57:56 -0300

    Corrige formatação da variável scheduler_token no arquivo de configuração

--- config/app.php
+++ config/app.php
@@ -84,7 +84,7 @@ return [
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
-    'scheduler_token' => env('SCHEDULER_TOKEN'),unidades
+    'scheduler_token' => env('SCHEDULER_TOKEN'),
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
