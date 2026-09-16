a83a0ed08cf255945139d323f5760c82c125c54d
Emanuel1138 <emanuelrcsousa@gmail.com>
2026-09-16 06:24:48 -0300

    adiciona campos ai_confidence e ai_reasoning na tabela committee_topic

--- app/Models/CommitteeTopic.php
+++ app/Models/CommitteeTopic.php
@@ -12,16 +12,12 @@ class CommitteeTopic extends Model
     protected $fillable = [
         'committee_id',
         'topic_id',
-        'match_score',
-        'match_method',
-        'reviewed',
-        'reviewed_at',
+        'ai_confidence',
+        'ai_reasoning',
     ];
 
     protected $casts = [
-        'reviewed' => 'boolean',
-        'reviewed_at' => 'datetime',
-        'match_score' => 'decimal:4',
+        'ai_confidence' => 'decimal:4',
     ];
 
     public function committee(): BelongsTo
